60d563605853b10279939fbf6444c8d7203eaf0c
Charaf Eddine BENARAB <30374045+EddCBen@users.noreply.github.com>
2024-06-14 14:29:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,24 @@ model.load_state_dict(torch.load("./models/Diabeties.pt"),
 
 model.eval()
 def classify_medical_data(BloodPressure : int,
-                   Glucose : int,
-                   BMI : float,
-                   age : int,
-                   pregnancies : int
-                   ):
+                          Glucose : int,
+                          BMI : float,
+                          age : int,
+                          pregnancies : int
+                          ):
     r"""
-    Classify the Sensory Data into: S7i7, or mrid
+    Classify the Sensory Data into: sick, or healthy
         args: Sensory Data 
         return: Class_ 
     """
     input_ = torch.tensor([BloodPressure, Glucose, BMI, age, pregnancies])
     class_ = model(input_.reshape(1,-1).float())
-    output = ["S7i7", "Mrid"][int(class_.squeeze().item())] 
+    output = ["sick", "healthy"][int(class_.squeeze().item())] 
     return output
 
 if __name__ == "__main__":
-    # Test, The model is crap, because the data is crap
     # Diabeties.pt -> The Data was manually agumented
     # Diabeties__1.pt -> Original Dataset (Better results)
-
     print(classify_medical_data(BloodPressure=120,
                                 Glucose=148,
                                 BMI=33.6,
